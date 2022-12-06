0091992b8141abd28f998775c40bc1f6caf5580b
janeirungu <jwirungu96@gmail.com>
2022-12-06 22:37:15 +0300

    Responsive React Front-end

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Brand,CTA, Navbar } from './components'
+import { Footer, Blog, Possibility, Features, Header, WhatGPT3 } from './containers'
+import './App.css'
+
+
+const App = () => {
+  return (
+    <div className= "App">
+
+      <div className='gradient__bg'>    
+  
+        <Navbar/>
+        <Header/>    
+    
+    </div>     
+     <Brand/>
+     <WhatGPT3/>       
+    <Features/>    
+     <Possibility/>
+     <CTA/>
+     <Blog/> 
+     <Footer/>
+    </div>
+  )
+}
+
+export default App
