e8ca50462d8f0831636b01ece83fdf75507e6861
Janeirungu01 <jwirungu96@gmail.com>
2025-01-26 08:31:12 +0300

    Update blog text

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -11,13 +11,13 @@ const Blog = () => {
       </div>
       <div className='gpt3__blog-container'>
         <div className='gpt3__blog-container_groupA'>
-        < Article imgUrl = {blog01} date='Sep 26, 2022' title = 'GPT-3 and Open  AI is the future. Let us exlore how it is?' />          
+        <Article imgUrl = {blog01} date='Sep 26, 2022' title = 'GPT-3 and Open  AI is the future. Let us explore how it is?' />          
         </div>
         <div className='gpt3__blog-container_groupB'>  
-        < Article imgUrl = {blog02} date='Sep 26, 2022' title = 'GPT-3 and Open  AI is the future. Let us exlore how it is?' />
-        < Article imgUrl = {blog03} date='Sep 26, 2022' title = 'GPT-3 and Open  AI is the future. Let us exlore how it is?' />
-        < Article imgUrl = {blog04} date='Sep 26, 2022' title = 'GPT-3 and Open  AI is the future. Let us exlore how it is?' />
-        < Article imgUrl = {blog05} date='Sep 26, 2022' title = 'GPT-3 and Open  AI is the future. Let us exlore how it is?' />            
+        <Article imgUrl = {blog02} date='Sep 26, 2022' title = 'GPT-3 and Open  AI is the future. Let us explore how it is?' />
+        <Article imgUrl = {blog03} date='Sep 26, 2022' title = 'GPT-3 and Open  AI is the future. Let us explore how it is?' />
+        <Article imgUrl = {blog04} date='Sep 26, 2022' title = 'GPT-3 and Open  AI is the future. Let us explore how it is?' />
+        <Article imgUrl = {blog05} date='Sep 26, 2022' title = 'GPT-3 and Open  AI is the future. Let us explore how it is?' />            
         </div>
       </div>
     </div>
